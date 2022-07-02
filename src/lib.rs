78cfb931923d52dbee619b9009ff0b2231ce5efe
Elad Kaplan <kaplan.elad@gmail.com>
2022-07-02 22:11:03 +0300

    filter download release url by os and arch

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,6 @@
 //! upversion
 //!
 //!
-//! ## Get newest version from GitHub releases
-//! ```rust
-//! use upversion::vendors::GitHubVendor;
-//! use upversion::VersionContext;
-//!
-//! fn main() {
-//!     let github_client = Box::new(GitHubVendor::new("owner", "repo-name"));
-//!     let version_context = VersionContext::new("app-name", github_client);
-//!     let version_template = version_context.run("0.0.5");
-//!     if let Some(new_version) = version_template {
-//!         println!("{}", new_version);
-//!     };
-//! }
-//! ```
 mod context;
 mod data;
 mod template;
